80f341afb7846e1da3b9a9c13fad39d877d9b772
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-12-13 10:15:22 -0500

    nice one

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,10 +1,30 @@
 public class MyLinkedList{
- private int size;
+ private int length;
  private Node start,end;
+
+ public MyLinkedList() {
+
+ }
 
  public int size();
 
  public boolean add(int value);
- 
- public String toString();
+
+ public String toString() {
+
+ }
+
+ public int get(int index) {
+   if (index < 0 || index >= length) {
+     throw new IndexOutOfBoundsException("Index less than 0, or greater than/equal to size.");
+   }
+
+
+
+
+ }
+
+ public Integer set(int index, Integer value) {
+   
+ }
 }
