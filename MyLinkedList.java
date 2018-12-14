b07f7764ec89b1fcadc7b5603564f84789ac0b6f
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-12-14 10:13:46 -0500

    lab for Friday.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -3,20 +3,79 @@ public class MyLinkedList{
  private Node start,end;
 
  public MyLinkedList() {
-
+   length = 0;
  }
 
- public int size();
+ public int size() {
+   return size;
+ }
 
- public boolean add(int value);
+ public boolean add(int value) {
+   Node x;
+   if (length == 0) {
+   x = new Node(0, null, null);
+   start = x;
+   end = x;
+   //there's only one node so!
+   length++;
+   return false;
+ }
+ else {
+   x = new Node(length, null, end);
+   end.setNext(x);
+   //gets the nodes to open up the null for new node.
+   end = x;
+   length++;
+   return false;
+ }
+}
 
+ public void add(int index,Integer value){
+
+
+ }
+ public boolean contains(Integer value){
+   Node x;
+   for (int i = 0; i < length; i++) {
+     x = new Node(length, end, start);
+     return x.contains(value);
+   }
+ }
+ public int indexOf(Integer value){
+
+ }
+ public Integer remove(int index) {
+
+ }
+ public boolean remove(Integer value) {
+
+ }
  public String toString() {
-
+   if (size == 0) {
+     return "[]";
+   }
+   String result = "["
+   Node current = start;
+   while (current != null) {
+     result += current.getData() + ", ";
+     current = current.next();
  }
+ return result + current.getData() + "]";
+}
 
- public int get(int index) {
+ public Integer get(int index) {
    if (index < 0 || index >= length) {
      throw new IndexOutOfBoundsException("Index less than 0, or greater than/equal to size.");
+   }
+   Node current = start;
+   for (int x = 0; x < length; x++) {
+     if (x = index){
+       return start.getData();
+     }
+     else {
+       x = (index, end, start)
+
+     }
    }
 
 
@@ -25,6 +84,6 @@ public class MyLinkedList{
  }
 
  public Integer set(int index, Integer value) {
-   
+
  }
 }
--- Node.java
+++ Node.java
@@ -2,39 +2,50 @@ private class Node{
  private int data;
  private Node next,prev;
 
+public Node(Intger value) {
+  data = value;
+  //another constructor was made because it could give you a data but not anything else.
+}
  public Node(Integer value, Node nex, Node pre) {
    data = value;
    next = nex;
-   prev = pre;   
+   prev = pre;
+   //constructor
  }
 
-private Node next(){
+public Node next(){
   return next;
+  //returns next
 }
 
-private Node prev(){
+public Node prev(){
   return prev;
+  //returns prev
 }
 
 private void setNext(Node other){
   next = other;
+  //sets next
 }
 
 private void setPrev(Node other){
   prev = other;
+  //sets prev
 }
 
-private Integer getData() {
+public Integer getData() {
   return data;
+  //returns the data.
 }
 
-private Integer setData(Integer i) {
+public Integer setData(Integer i) {
   Integer temp = data;
   data = i;
   return temp;
+  //stores it a variable and switches up data and then returns the variable.
 }
 
 public String toString() {
-  return "" + data;
+  return " " + data;
 }
 }
